c449c4c582c4fb6216e4b130a76299173e697a4d
webdevfstackeng <u175797830@byui.edu>
2023-11-07 16:05:07 +0200

    Week01 Commit

--- w01-task.js
+++ w01-task.js
@@ -13,7 +13,7 @@ console.log(`It is now ${currentDateAndTime}`);
 let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(`The total is ${theTotal}`);
 
-// 'total' function declaration
+//'total' function declaration
 function total(...theNumbers) {
 	let sum = 0;
 	for (let aNumber in theNumbers) {
